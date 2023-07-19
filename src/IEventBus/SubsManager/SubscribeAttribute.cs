b659baf6bc9c9b9322ca79245747bc65ebdf32ab
zhangxiancheng <xian_cheng@ckc.creative.com>
2023-07-19 16:36:08 +0800

    EventBus

---
+++ src/IEventBus/SubsManager/SubscribeAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EventBus.SubsManager
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class SubscribeAttribute : Attribute
+    {
+        public string Topic { get; set; }
+        public SubscribeAttribute(string topic)
+        {
+            Topic = topic;
+        }
+    }
+}
